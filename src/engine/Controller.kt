0a5fa604566d56d86c0c8f9243d0906d4a32e490
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-11-07 13:25:33 +0100

    Move QuizService interface to controller

--- src/engine/Controller.kt
+++ src/engine/Controller.kt
@@ -68,3 +68,10 @@ private fun Pair<UInt, Quiz>.toDto() = QuizOutDto(
     text = second.text,
     options = second.options,
 )
+
+interface QuizService {
+    fun getQuiz(): Pair<UInt, Quiz>
+    fun checkAnswer(answerIdx: Int): AnswerResult
+    fun addQuiz(quiz: Quiz): Pair<UInt, Quiz>
+    fun getQuizWith(id: UInt): Pair<UInt, Quiz>
+}
