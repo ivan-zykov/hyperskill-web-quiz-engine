1c08f9a0b2dd20690e4bdbbcf942c6809c6ad470
Ivan Zykov <ivan.v.zykov@gmail.com>
2026-02-20 17:01:36 +0100

    Add entity for Quiz

---
+++ src/engine/QuizEntity.kt
@@ -0,0 +1,29 @@
+package engine
+
+import jakarta.persistence.*
+import org.hibernate.annotations.Fetch
+import org.hibernate.annotations.FetchMode
+
+@Entity
+@Table(name = "quizzes")
+class QuizEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null
+
+    @Column(name = "title")
+    var title: String? = null
+
+    @Column(name = "text")
+    var text: String? = null
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Fetch(value = FetchMode.SUBSELECT)
+    @Column(name = "options")
+    var options: List<String>? = null
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "answers")
+    var answers: List<Int>? = null
+}
