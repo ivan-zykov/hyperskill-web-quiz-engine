aa9651b0220bc337456755985d05a302c091edb9
Ivan Zykov <ivan.v.zykov@gmail.com>
2026-04-24 15:56:10 +0200

    Remove in-memory quiz repo from service test

--- test/engine/QuizServiceImplTest.kt
+++ test/engine/QuizServiceImplTest.kt
@@ -6,37 +6,49 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.context.ActiveProfiles
 
 private const val CONGRATULATIONS = "Congratulations, you're right!"
 private const val WRONG_ANSWER = "Wrong answer! Please, try again."
 
+private const val USERNAME = "test@user.com"
+private const val PASSWORD = "testPass"
+
 @DataJpaTest
-@ActiveProfiles("test")
-class QuizServiceImplTest @Autowired constructor(private val userRepo: AppUserRepository) {
-    private lateinit var sut: QuizServiceImpl
+@Import(JpaQuizzesRepositoryAdapter::class)
+@ActiveProfiles("jpa", "test")
+class QuizServiceImplTest @Autowired constructor(
+    private val userRepo: AppUserRepository,
+    quizRepo: QuizzesRepository,
+) {
+    private val passEncoder: PasswordEncoder = BCryptPasswordEncoder(7)
+    private val sut = QuizServiceImpl(
+        quizRepo,
+        userRepo,
+        passEncoder
+    )
 
     private val user = AppUser(
         id = 1,
-        username = "test@user.com",
-        password = "testPass"
+        username = USERNAME,
+        password = PASSWORD
+    )
+    private val encodedUser = AppUser(
+        username = USERNAME,
+        password = passEncoder.encode(PASSWORD)
     )
     private val userDetails: UserDetails = AppUserAdapter(user)
 
     @BeforeEach
     fun setUp() {
-        val quizzesRepository = InMemoryQuizzesRepository()
-        sut = QuizServiceImpl(
-            quizzesRepository,
-            userRepo,
-            BCryptPasswordEncoder(7)
-        )
+        userRepo.save(encodedUser)
     }
 
-    private val quiz1Id = QuizId(1)
     private val newQuiz1 = NewQuiz(
         title = "The Java Logo",
         text = "What is depicted on the Java logo?",
@@ -51,15 +63,14 @@ class QuizServiceImplTest @Autowired constructor(private val userRepo: AppUserRe
 
     @Test
     fun `Gets initial quiz`() {
-        val actual = sut.getInitialQuiz(userDetails)
+        val actualQuiz = sut.getInitialQuiz(userDetails)
 
         assertAll(
-            { assertEquals(quiz1Id, actual.id) },
-            { assertEquals(newQuiz1.title, actual.title) },
-            { assertEquals(newQuiz1.text, actual.text) },
-            { assertEquals(newQuiz1.options, actual.options) },
-            { assertEquals(newQuiz1.answer, actual.answer) },
-            { assertEquals(user.username, actual.authorUsername) }
+            { assertEquals(newQuiz1.title, actualQuiz.title) },
+            { assertEquals(newQuiz1.text, actualQuiz.text) },
+            { assertEquals(newQuiz1.options, actualQuiz.options) },
+            { assertEquals(newQuiz1.answer, actualQuiz.answer) },
+            { assertEquals(user.username, actualQuiz.authorUsername) }
         )
     }
 
@@ -77,31 +88,30 @@ class QuizServiceImplTest @Autowired constructor(private val userRepo: AppUserRe
 
     @Test
     fun `Adds a quiz`() {
-        val actual = sut.addQuiz(newQuiz = newQuiz1, userDetails = userDetails)
+        val actualQuiz = sut.addQuiz(newQuiz = newQuiz1, userDetails = userDetails)
 
         assertAll(
-            { assertEquals(quiz1Id, actual.id) },
-            { assertEquals(newQuiz1.title, actual.title) },
-            { assertEquals(newQuiz1.text, actual.text) },
-            { assertEquals(newQuiz1.options, actual.options) },
-            { assertEquals(newQuiz1.answer, actual.answer) },
-            { assertEquals(user.username, actual.authorUsername) }
+            { assertEquals(newQuiz1.title, actualQuiz.title) },
+            { assertEquals(newQuiz1.text, actualQuiz.text) },
+            { assertEquals(newQuiz1.options, actualQuiz.options) },
+            { assertEquals(newQuiz1.answer, actualQuiz.answer) },
+            { assertEquals(user.username, actualQuiz.authorUsername) }
         )
     }
 
     @Test
     fun `Gets quiz by ID`() {
-        sut.addQuiz(newQuiz1, userDetails)
+        val savedQuiz = sut.addQuiz(newQuiz1, userDetails)
 
-        val actual = sut.getQuizBy(id = quiz1Id)
+        val fetchedQuiz = sut.getQuizBy(id = savedQuiz.id)
 
         assertAll(
-            { assertEquals(quiz1Id, actual.id) },
-            { assertEquals(newQuiz1.title, actual.title) },
-            { assertEquals(newQuiz1.text, actual.text) },
-            { assertEquals(newQuiz1.options, actual.options) },
-            { assertEquals(newQuiz1.answer, actual.answer) },
-            { assertEquals(user.username, actual.authorUsername) }
+            { assertEquals(savedQuiz.id, fetchedQuiz.id) },
+            { assertEquals(savedQuiz.title, fetchedQuiz.title) },
+            { assertEquals(savedQuiz.text, fetchedQuiz.text) },
+            { assertEquals(savedQuiz.options, fetchedQuiz.options) },
+            { assertEquals(savedQuiz.answer, fetchedQuiz.answer) },
+            { assertEquals(user.username, fetchedQuiz.authorUsername) }
         )
     }
 
@@ -114,27 +124,26 @@ class QuizServiceImplTest @Autowired constructor(private val userRepo: AppUserRe
 
     @Test
     fun `Gets two quizzes`() {
-        sut.addQuiz(newQuiz = newQuiz1, userDetails = userDetails)
+        val savedQuiz1 = sut.addQuiz(newQuiz = newQuiz1, userDetails = userDetails)
         val newQuiz2 = newQuiz1.copy(title = "The Java Logo 2")
-        val quiz2Id = QuizId(2)
-        sut.addQuiz(newQuiz = newQuiz2, userDetails = userDetails)
+        val savedQuiz2 = sut.addQuiz(newQuiz = newQuiz2, userDetails = userDetails)
 
-        val actual = sut.getAllQuizzes()
+        val fetchedQuizzes = sut.getAllQuizzes()
 
         assertAll(
-            { assertEquals(2, actual.size) },
-            { assertEquals(quiz1Id, actual[0].id) },
-            { assertEquals(newQuiz1.title, actual[0].title) },
-            { assertEquals(newQuiz1.text, actual[0].text) },
-            { assertEquals(newQuiz1.options, actual[0].options) },
-            { assertEquals(newQuiz1.answer, actual[0].answer) },
-            { assertEquals(user.username, actual[0].authorUsername) },
-            { assertEquals(quiz2Id, actual[1].id) },
-            { assertEquals(newQuiz2.title, actual[1].title) },
-            { assertEquals(newQuiz2.text, actual[1].text) },
-            { assertEquals(newQuiz2.options, actual[1].options) },
-            { assertEquals(newQuiz2.answer, actual[1].answer) },
-            { assertEquals(user.username, actual[1].authorUsername) },
+            { assertEquals(2, fetchedQuizzes.size) },
+            { assertEquals(savedQuiz1.id, fetchedQuizzes[0].id) },
+            { assertEquals(savedQuiz1.title, fetchedQuizzes[0].title) },
+            { assertEquals(savedQuiz1.text, fetchedQuizzes[0].text) },
+            { assertEquals(savedQuiz1.options, fetchedQuizzes[0].options) },
+            { assertEquals(savedQuiz1.answer, fetchedQuizzes[0].answer) },
+            { assertEquals(user.username, fetchedQuizzes[0].authorUsername) },
+            { assertEquals(savedQuiz2.id, fetchedQuizzes[1].id) },
+            { assertEquals(savedQuiz2.title, fetchedQuizzes[1].title) },
+            { assertEquals(savedQuiz2.text, fetchedQuizzes[1].text) },
+            { assertEquals(savedQuiz2.options, fetchedQuizzes[1].options) },
+            { assertEquals(savedQuiz2.answer, fetchedQuizzes[1].answer) },
+            { assertEquals(user.username, fetchedQuizzes[1].authorUsername) },
         )
     }
 
@@ -204,7 +213,7 @@ class QuizServiceImplTest @Autowired constructor(private val userRepo: AppUserRe
         val quiz = sut.getAllQuizzes().first()
         val otherUser = AppUser(
             username = "other@user.com",
-            password = "testPass"
+            password = PASSWORD
         )
         val otherUserDetails = AppUserAdapter(otherUser)
 
